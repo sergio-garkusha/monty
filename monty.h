5ad8ec26749132bb70faac1003cfa0aa144e5803
Cu7ious <328@holbertonschool.com>
2018-04-24 07:40:57 +0000

    Added opcode interface

--- monty.h
+++ monty.h
@@ -17,4 +17,18 @@ typedef struct stack_s
         struct stack_s *next;
 } stack_t;
 
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 #endif
