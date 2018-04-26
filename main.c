b2669ebbd7537df770968eda2eaa656af6077544
Cu7ious <328@holbertonschool.com>
2018-04-26 22:59:24 +0000

    Refactired code, decoupled it to functions

--- main.c
+++ main.c
@@ -1,89 +1,17 @@
 #include "monty.h"
 
-void push(stack_t **stack, unsigned int line_number)
-{
-	stack_t *node, *temp;
+stack_t **global_head;
 
-	if (*stack == NULL)
-	{
-		*stack = malloc(sizeof(stack_t));
-		if (*stack == NULL)
-		{
-			printf("Error: malloc failed\n");
-			exit(EXIT_FAILURE);
-		}
-
-		(*stack)->n = line_number;
-		(*stack)->prev = NULL;
-		(*stack)->next = NULL;
-	}
-	else
-	{
-		node = malloc(sizeof(stack_t));
-		if (node == NULL)
-		{
-			printf("Error: malloc failed\n");
-			exit(EXIT_FAILURE);
-		}
-
-		temp = *stack;
-		while(temp->next)
-			temp = temp->next;
-
-		node->n = line_number;
-		node->prev = temp;
-		node->next = NULL;
-
-		temp->next = node;
-	}
-}
-
-void pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp = *stack;
-
-	(void) line_number;
-
-	while(temp)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-	}
-}
-
-void parse_command(stack_t **stack, char *op, unsigned int line_num)
-{
-	int i;
-	instruction_t ops[] = {
-		{"push", push},
-		{"pall", pall},
-		{NULL, NULL}
-	};
-
-	for (i = 0; ops[i].opcode; i++)
-		if (strcmp(op, ops[i].opcode) == 0)
-		{
-			ops[i].f(stack, line_num);
-			return;
-		}
-
-	if (strlen(op) != 0)
-	{
-		printf("L%u: unknown instruction %s\n", line_num, op);
-		exit(EXIT_FAILURE);
-	}
-}
-
+/**
+ * main - the root of the project
+ * @argc: how many arguments were passed to a program
+ * @argv: arguments vector
+ *
+ * Return: on scuccess, always EXIT_SUCCESS
+ */
 int main(int argc, char *argv[])
 {
 	stack_t *head;
-
-	size_t len;
-	ssize_t read_line;
-	unsigned int line_num = 0;
-	char *line = NULL;
-	FILE *fd;
-	char *command;
 
 	if (argc != 2)
 	{
@@ -91,26 +19,10 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	fd = fopen(argv[1], O_RDONLY);
-	if (!fd)
-	{
-		printf("Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+	global_head = &head;
+	read_file(argv[1], &head);
 
-	while ((read_line = getline(&line, &len, fd)) != -1)
-	{
-		command = strtok(line, DELIMS);
-		line_num++;
+	atexit(global_free);
 
-		if (command)
-			parse_command(&head, command, line_num);
-	}
-
-	if (line)
-		free(line);
-
-	fclose(fd);
-
-	exit(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
