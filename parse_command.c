e0cd7a5de83daeb06608d3e418d8a928679e56e1
Cu7ious <328@holbertonschool.com>
2018-04-26 22:56:52 +0000

    Parses the commands from the line of the op file

---
+++ parse_command.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * parse_command - parses commands from the line op
+ * @stack: the pointer to the head of the stack
+ * @op: the line with commands/instructions
+ * @line_num: a number of the line
+ *
+ * Return: void
+ */
+void parse_command(stack_t **stack, char *op, unsigned int line_num)
+{
+	int i;
+	instruction_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+
+	for (i = 0; ops[i].opcode; i++)
+		if (strcmp(op, ops[i].opcode) == 0)
+		{
+			ops[i].f(stack, line_num);
+			return;
+		}
+
+	if (strlen(op) != 0)
+	{
+		printf("L%u: unknown instruction %s\n", line_num, op);
+		exit(EXIT_FAILURE);
+	}
+}
