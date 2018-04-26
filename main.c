36cb86a2987dcb4e0536217937007d0b0ed47669
Cu7ious <328@holbertonschool.com>
2018-04-26 06:37:21 +0000

    Added ability to open opcode files

--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 
 int main(int argc, char *argv[])
 {
+	int fd;
+
 	if (argc != 2)
 	{
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
 	}
 
 	(void) argv;
