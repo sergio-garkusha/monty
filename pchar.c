853303c9cf01ad15c38a343a3b9524407667b63a
Cu7ious <328@holbertonschool.com>
2018-04-27 05:37:11 +0000

    Implemented the pchar opcode

---
+++ pchar.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * pchar - prints the char at the top of the stack, followed by a new line.
+ * @stack: double pointer to the head of the stack
+ * @line_number: the number of the line in file
+ *
+ * Return: void
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	int num;
+
+	if (!stack || !(*stack))
+	{
+		printf("L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	num = (*stack)->n;
+
+	if (num < 0 || num > 127)
+	{
+		printf("L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", num);
+}
