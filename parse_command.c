34eca943e34872c8add587e7e04ad319b88e71d8
Cu7ious <328@holbertonschool.com>
2018-04-27 05:22:04 +0000

    Changed the check of a unknown instruction

--- parse_command.c
+++ parse_command.c
@@ -33,7 +33,7 @@ void parse_command(stack_t **stack, char *op, unsigned int line_num)
 			return;
 		}
 
-	if (strlen(op) != 0)
+	if (strlen(op) != 0 && op[0] != '#')
 	{
 		printf("L%u: unknown instruction %s\n", line_num, op);
 		exit(EXIT_FAILURE);
