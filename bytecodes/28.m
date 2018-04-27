ba02bd8207922bf893776880d2a02cd84223d7af
Cu7ious <328@holbertonschool.com>
2018-04-27 07:14:45 +0000

    Added different test files to test the opcodes

---
+++ bytecodes/28.m
@@ -0,0 +1,11 @@
+# H char code
+push 72
+pchar
+
+# invalid char
+# push -1
+# pchar
+
+# invalid char
+push 300
+pchar
--- bytecodes/43.m
+++ bytecodes/43.m
@@ -1,12 +1,27 @@
+# let's start with 8 & 4
 push 8
 push 4
 
+# check that we have exactly 8 & 4
+pall
+
+# divide 8 by 4
 div
-
+# print the result, should be 2
 pall
 
+# add another 8
 push 8
-
+# multiply 8 by 2
 mul
-
 pall
+# print the result, should be 16
+
+# adds 7 to the stack
+push 7
+pall
+
+# performs 16 % 7
+mod
+pall
+# print the result, should be ~2
