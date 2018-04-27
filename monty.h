9ee2fe0b8d33d63999fea09875e77785c0997215
Cu7ious <328@holbertonschool.com>
2018-04-27 05:35:56 +0000

    Implemented the pchar opcode

--- monty.h
+++ monty.h
@@ -60,5 +60,6 @@ void sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
 
 #endif
--- parse_command.c
+++ parse_command.c
@@ -23,6 +23,7 @@ void parse_command(stack_t **stack, char *op, unsigned int line_num)
 		{"div", _div},
 		{"mul", mul},
 		{"mod", mod},
+		{"pchar", pchar},
 		{NULL, NULL}
 	};
 
